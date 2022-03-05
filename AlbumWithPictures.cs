e97b2e772efee19470fd761ad721bbe247ccbf7b
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-06 01:44:12 +0300

    Add files via upload

---
+++ AlbumWithPictures.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityPictures = 52;
+            int quantityColumns = 3;
+            int numbersColumns = quantityPictures / quantityColumns;
+            int overColumnsQuantity = quantityPictures - (numbersColumns * quantityColumns);
+            Console.WriteLine($"Количество: \n  картинок в альбоме - {quantityPictures} \n  рядов картинок - {quantityColumns} \n  " +
+                $"целых рядов - {numbersColumns} \n  картинок сверх альбома - {overColumnsQuantity}");
+        }
+    }
+}
