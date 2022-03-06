9a281316a2219f575e35251d92b764732dba6e16
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-06 16:47:31 +0300

    Update AlbumWithPictures.cs

--- AlbumWithPictures.cs
+++ AlbumWithPictures.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp9
         {
             int pictures = 55;
             int picturesInRow = 3;
-            int totalRows = pictures / picturesInRow;
+            int filledRows = pictures / picturesInRow;
             int overPictures = pictures % picturesInRow;
             Console.WriteLine($"Количество: \n  картинок в альбоме - {pictures} \n  картинок в ряду - {picturesInRow} \n  " +
             $"целых рядов - {totalRows} \n  картинок сверх альбома - {overPictures}");
