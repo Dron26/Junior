fee40c40e1bc1fe82fa23edf111ce63d6f1836fc
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-06 12:57:10 +0300

    Update CellerOfDiamonds.cs

--- CellerOfDiamonds.cs
+++ CellerOfDiamonds.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp9
 {
@@ -6,24 +6,23 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
-            int quantityMyGold;
-            int quantityMyDiamonds;
+            int gold;
+            int diamondsCount;
             int priceDiamond = 45;
-            int quantityBoughtDiamonds;
+            
             Console.WriteLine("Вы собираетесь посетить магазин и преобрести кристалы");
             Console.WriteLine("Введите количество золота которое возьмете с собой:");
-            quantityMyGold = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Поздравляю! В ваш кошелек успешно добавлено {quantityMyGold} золотых монет");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Поздравляю! В ваш кошелек успешно добавлено {gold} золотых монет");
             Console.WriteLine("Теперь пора навестить магазин!");
             Console.ReadLine();
             Console.Clear();
             Console.WriteLine($"Вы заходите в магазин и вас приветствует (П)родавец \n  " +
                 $"(П):  Добрый день! В нашем магазине самые качественные кристалы, сколько вы хотели бы приобрести кристалов? \n    Предупрежу 1 аристал стоит:  {priceDiamond} золотых монет");
             Console.WriteLine("Введите количество кристалов к покупке: ");
-            quantityBoughtDiamonds = Convert.ToInt32(Console.ReadLine());
-            quantityMyDiamonds = quantityMyGold - (priceDiamond * quantityBoughtDiamonds);
-            quantityMyGold -= priceDiamond * quantityBoughtDiamonds;
-            Console.WriteLine($"Поздравляю! Вы успешно приобрели {quantityBoughtDiamonds} и у вас еще осталось {quantityMyGold} золотых монет!");
+            diamondsCount = Convert.ToInt32(Console.ReadLine());            
+            gold -= priceDiamond * diamondsCount;
+            Console.WriteLine($"Поздравляю! Вы успешно приобрели кристалы в количестве {diamondsCount}шт и у вас еще осталось {gold} золотых монет!");
         }
     }
 }
