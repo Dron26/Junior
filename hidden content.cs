8c947dd4f6d8a3784f3395586be0c5ba3e91f80a
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-10 01:09:19 +0300

    Add files via upload

---
+++ hidden content.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "Admin";
+            string inputUser;
+            bool isWorker =true;
+            int inputCount=0;
+
+            while (isWorker&&inputCount<3)
+            {
+                Console.WriteLine("Введите пароль для доступа к почте: ");
+                inputUser = Console.ReadLine();
+                if (inputUser == password)
+                {
+                    Console.WriteLine("  Пароль верен\n  Секректное сообщение: Нычка в системнике, проверь");
+                    Console.ReadLine();
+                    isWorker = false;
+                }
+
+                else if (inputUser != password)
+                {
+                    Console.WriteLine($"  Введен неверный пароль!\n  Осталось {2-inputCount} попыток ввода ");
+                    inputCount++;
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }           
+        }
+    }
+}
+
