ccde4513861bc40f87ba305e7d3d539bd50818c3
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-09 05:58:40 +0300

    Add files via upload

---
+++ SymbolWithText.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace СимволыВокругТекста
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, symbol;
+            int nameLenght;
+
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+            nameLenght = name.Length;
+            Console.WriteLine("Введите символ");
+            symbol = Console.ReadLine();
+            Console.Clear();
+
+            for
+                (int i = 0; i <= nameLenght + 1; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine("");
+            Console.Write(symbol + name + symbol);
+            Console.WriteLine("");
+
+            for
+                (int i = 0; i <= nameLenght + 1; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.ReadLine();
+        }
+    }
+}
