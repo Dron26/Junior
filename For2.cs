b5ffb7a3fad1efff0b57e400e0a0d9324958a932
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-09 15:06:20 +0300

    Update For2.cs

--- For2.cs
+++ For2.cs
@@ -1,19 +1,18 @@
-﻿using System;
+using System;
 
 namespace sequence
 {
     internal class Program
     {
-        //выбрал цикл for так как есть условия по счетчику, просто и удобно.
         static void Main(string[] args)
         {
             int maxValue = 98;
-            int j = 0;
-            for (int i = 0;j<maxValue; i++)
-            {               
-                j =j+ 7;
-                Console.Write($" {j} ");
-            }            
+            int minValue = 7;
+            for (int i = minValue; i<maxValue;)
+            {
+                i = i + minValue;
+                Console.Write($" {i} ");
+            }
         }
     }
 }
