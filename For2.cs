1b67731e63784b9e3d4f49e40aaea02d0069c7a4
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-09 05:42:50 +0300

    Add files via upload

---
+++ For2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sequence
+{
+    internal class Program
+    {
+        //выбрал цикл for так как есть условия по счетчику, просто и удобно.
+        static void Main(string[] args)
+        {
+            int maxValue = 98;
+            int j = 0;
+            for (int i = 0;j<maxValue; i++)
+            {               
+                j =j+ 7;
+                Console.Write($" {j} ");
+            }            
+        }
+    }
+}
