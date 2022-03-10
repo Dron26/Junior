ae89d610e17991a22d8afb6f4c227fbea6f063dd
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-10 23:49:11 +0300

    Add files via upload

---
+++ TwoVariables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TwoVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name= "Chetnikov";
+            string surname= "Andrey";
+
+            Console.WriteLine($"{name}  {surname}");
+            (name, surname) = (surname, name);
+            Console.WriteLine($"{name}  {surname}");
+        }
+    }
+}
