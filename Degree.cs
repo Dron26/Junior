5d91af17d06cb9a8ea337ded7c4e98ab4ce91de4
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-11 02:49:02 +0300

    Add files via upload

---
+++ Degree.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+
+namespace converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 2;
+            int maxNumber = 27;
+            int degree = 0;
+            Random random = new Random();
+            int number = random.Next(Number, maxNumber);
+            int upDegree = 1;
+            bool isWorking = true;
+
+            while (isWorking)
+            {               
+                if (upDegree > number)
+                {
+                    isWorking = false;
+                }
+
+                else if (upDegree < number)
+                {
+                    upDegree = upDegree * Number;
+                    degree++;
+                }
+            }
+            Console.WriteLine($"{upDegree} - это результат возведения числа {Number}  в степень {degree}");
+        }
+    }
+}
