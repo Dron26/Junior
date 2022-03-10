a63d2fe4c961fa859f797c23cb55b19d2dea6473
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-11 02:21:10 +0300

    Add files via upload

---
+++ multiplicity.cs
@@ -0,0 +1,38 @@
+﻿
+    
+using System;
+
+namespace converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 1;
+            int maxNumber = 27;
+            int maxValue = 999;
+            int minValue = 99;
+            Random random = new Random();
+            int countNumber = 0;
+            int number = random.Next(minNumber, maxNumber);
+            int upNumber=number;
+            bool isWorking = true;
+
+            while (isWorking)
+            {
+                upNumber+=number;
+
+                if (upNumber >= maxValue)
+                {
+                    isWorking = false;
+                }
+
+                else if ( upNumber>minValue )
+                {
+                    countNumber++;                    
+                }
+            }
+            Console.WriteLine($"{countNumber} трехзначных чисел кратны {number}" ) ;            
+        }
+    }
+}
