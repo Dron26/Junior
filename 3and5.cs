4ea3408ec229d8b259b6ae1c77b2cf58624b301f
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-11 00:03:09 +0300

    Add files via upload

---
+++ 3and5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TwoVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(0, 100);
+            int summ = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 3 == 0 | i % 5 == 0)
+                {
+                    summ = summ + i;
+                }
+            }
+            Console.WriteLine(summ);
+        }
+    }
+}
