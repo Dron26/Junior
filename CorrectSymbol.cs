2ec2f131e0a92c4d565c87446605a7790a821d58
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-11 05:27:36 +0300

    Add files via upload

---
+++ CorrectSymbol.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string textInput;
+            int upDepth = 0;
+            int downdDepth = 0;
+            int maxDepth = 0;
+            int clouseDepth = 1;
+            int upDepthAll = 1;
+            bool CorrectText = false;
+            string showCorrectText = null;
+            int count = 0;
+
+            Console.WriteLine("Введите комбинацию символов");
+            textInput=Console.ReadLine();
+
+            foreach (char c in textInput)
+            {             
+                if (c==')')
+                {
+                    downdDepth++;
+                    if (c == ')'&(count == 0| count == 1))
+                    {
+                        CorrectText = true;
+                    }
+                    
+                    else if (maxDepth< upDepth)
+                    {
+                        maxDepth = upDepth;
+                        upDepth= upDepth - clouseDepth;
+                    }
+
+                    else if (downdDepth> upDepthAll)
+                    {
+                        CorrectText = true;
+                    }
+                }
+
+                else if(count > 0&&c=='(')
+                {
+                    upDepth++;
+                    upDepthAll++;
+                }
+                count++;              
+            }
+            if (upDepthAll != downdDepth)
+            {
+                CorrectText = true;
+                showCorrectText = "не корректная";
+            }     
+            
+            else
+            {
+                showCorrectText = "корректная";
+            }
+            Console.WriteLine($"{textInput} - строка {showCorrectText}  и максимум глубины равняется {maxDepth}");
+        }    
+    } 
+}
