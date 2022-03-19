43ddc48c4cc87c71c24b2990b53b75002000e492
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-20 01:49:02 +0300

    Add files via upload

---
+++ LocalMax.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Massivi_zadanij
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxElements = 30;
+            int[] localArray = new int[maxElements];
+            int localMax = 0;
+            int LocalRange = 1;
+            int[] localM = new int[localArray.Length];
+            int o;
+
+            for (int i = 0; i < localArray.Length; i++)
+            {
+                localArray[i] = random.Next(0, 9);
+                Console.Write(localArray[i] + "  ");
+            }
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < localArray.Length; i++)
+            {
+
+                if (i == 0)
+                {
+                    if (localArray[i] > localArray[i + LocalRange])
+                    {
+                        localMax = localArray[i];
+                        Console.WriteLine(localMax);
+                    }
+                }
+                else if (i > 0 && i < localArray.Length - 1)
+                {
+                    if (localArray[i - LocalRange] <= localArray[i] && localArray[i] >= localArray[i + LocalRange])
+                    {
+                        localMax = localArray[i];
+                        Console.WriteLine(localMax);
+                    }
+                }
+                else
+                {
+                    if (localArray[i - LocalRange] <= localArray[i])
+                    {
+                        localMax = localArray[i];
+                        Console.WriteLine(localMax);
+                    }
+                }
+            }
+        }
+    }
+}
