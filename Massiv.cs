0cadb89adddedea5b77ec04190bd78ee931d0dea
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-20 00:40:30 +0300

    Update Massiv.cs

--- Massiv.cs
+++ Massiv.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Massiv
 {
@@ -9,42 +9,28 @@ namespace Massiv
             int[,] array = {
                 { 2, 1, 2, 3 },
                 { 2, 1, 5, 7 },
-                { 2, 3, 5, 2 } };
-            int summ = 0;
-            int productColumn=1;
+                { 2, 3, 5, 2 },
+                { 1, 7, 3, 5 }};
+            int sumLine = 0;
+            int productColumn = 1;
             int line = 2;
-            int column = 0;
+            int column = 1;
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.Write(array[i,j]);
+                    Console.Write(array[i, j]);
                 }
                 Console.WriteLine();
             }
 
-            for (int i = 1; i < line; i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    summ += array[i, j];
-                }
-            }
-          
             for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j == column; j++)
-                {
-                    productColumn *= array[i, j];
-                }
+            {                
+                productColumn *= array[i, column];
+                sumLine += array[line, i];                
             }
-            Console.WriteLine($"Сумма второй строки {summ}, произведение первого столбца {productColumn}");
+            Console.WriteLine($"Сумма второй строки {sumLine}, произведение первого столбца {productColumn}");
         }
     }
 }
-
-
-
-
-
