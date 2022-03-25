3e0c19279e052db7aead2cc1978a94a4c17e4b6d
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-26 02:19:58 +0300

    Add files via upload

---
+++ duplicate_Numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace duplicate_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxElements = 30;
+            int[] array = new int[maxElements];
+            int countDuplicate = 0;
+            int maxCountDuplicate=0;
+            int maxNumber = 0;
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 3);
+                Console.Write(array[i] + " ");
+            }
+            Console.Write("\n\n");
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int z=(array[i]);
+                if (array[i - 1] == array[i])
+                {
+                    countDuplicate++;
+                }
+                else
+                {
+                    if (maxCountDuplicate <= countDuplicate)
+                    {
+                        maxCountDuplicate = countDuplicate;
+                        maxNumber = array[i - 1];
+                    }
+                    countDuplicate = 0;
+                }    
+            }
+                Console.Write($" число {maxNumber}, имеет количество повторений = {maxCountDuplicate} ");
+        }
+    }
+}
