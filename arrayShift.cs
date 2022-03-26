9d53cf0319b7bfebc3b0d9d3526da80f56af8d10
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-26 06:29:00 +0300

    Update arrayShift.cs

--- arrayShift.cs
+++ arrayShift.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 
-namespace arrayShift
+namespace SortArray
 {
     internal class Program
     {
@@ -9,7 +9,6 @@ namespace arrayShift
             Random random = new Random();
             int maxElements = 10;
             int[] array = new int[maxElements];
-            int userInput;
 
             for (int i = 0; i < array.Length; i++)
             {
@@ -17,23 +16,17 @@ namespace arrayShift
                 Console.Write(array[i] + " ");
             }
             Console.Write("\n\n");
-            Console.WriteLine("Введите значение сдвига");
-            userInput = Convert.ToInt32(Console.ReadLine());
-            Console.Write("\n\n");
 
-            for (int i = 0; i < userInput; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                int number = array[0];
-                for (int j = 0; j < array.GetLength(0); j++)
-                {                 
-                    if (j != array.GetLength(0)-1)
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
                     {
-                        array[j] = array[j + 1];
-                    }
-                    else
-                    {
+                        int number = array[i];
+                        array[i] = array[j];
                         array[j] = number;
-                    }                   
+                    }
                 }
             }
 
@@ -44,4 +37,3 @@ namespace arrayShift
         }
     }
 }
-
