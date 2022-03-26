0dd3e0cdf23ec9fd5bfda221da07ad6b01cd7b74
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-03-26 05:30:13 +0300

    Add files via upload

---
+++ ArrayText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArrayText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Дана строка с текстом, используя метод строки String.Split() получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.";
+            string [] emptySpace = text.Split(" ");
+
+            foreach (var empety in emptySpace)
+            {
+                Console.WriteLine(empety);
+            }            
+        }
+    }
+}
