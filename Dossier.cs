2c04c99cd308431eee84f1e97ede55ab88a44064
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-04-11 00:05:22 +0300

    Update Dossier.cs

--- Dossier.cs
+++ Dossier.cs
@@ -1,7 +1,7 @@
 using System;
 
 namespace Cadrovui_ychet
-{ 
+{
     class Program
     {
         static void Main(string[] args)
@@ -44,8 +44,6 @@ namespace Cadrovui_ychet
 
         static void CreatDossier(ref string[] fullNames, ref string[] positions)
         {
-            string[] tempFamily = new string[fullNames.Length + 1];
-            string[] tempPost = new string[positions.Length + 1];
             string names;
             string post;
 
@@ -58,18 +56,22 @@ namespace Cadrovui_ychet
             names += (Console.ReadLine() + " - ").ToUpper();
             Console.WriteLine("\n  Введите Должность:\n");
             post = (Console.ReadLine()).ToUpper();
-
-            for (int i = 0; i < fullNames.GetLength(0); i++)
-            {
-                tempFamily[i] = fullNames[i];
-                tempPost[i] = positions[i];
-            }
-            tempFamily[tempFamily.Length - 1] = names;
-            tempPost[tempPost.Length - 1] = post;
-            fullNames = tempFamily;
-            positions = tempPost;
+            ExtensionArray(ref fullNames, names);
+            ExtensionArray(ref positions, post);
             Console.WriteLine("Досье успешно добавлено!");
             Console.ReadKey();
+        }
+
+        static void ExtensionArray(ref string[] extension, string inputData)
+        {
+            string[] tempExtension = new string[extension.Length + 1];
+
+            for (int i = 0; i < extension.GetLength(0); i++)
+            {
+                tempExtension[i] = extension[i];
+            }
+            tempExtension[tempExtension.Length - 1] = inputData;
+            extension = tempExtension;
         }
 
         static void ShowAllDossiers(string[] fullNames, string[] positions)
@@ -100,38 +102,31 @@ namespace Cadrovui_ychet
 
             if (userInput > 0 & userInput <= fullNames.Length)
             {
-                string[] tempFamily = new string[fullNames.Length];
-                string[] tempFamilyDown = new string[tempFamily.Length - 1];
-                string[] tempPost = new string[positions.Length];
-                string[] tempPostDown = new string[tempPost.Length - 1];
-
-                for (int i = 0; i < userInput; i++)
-                {
-                    tempFamily[i] = fullNames[i];
-                    tempPost[i] = positions[i];
-                }
-
-                for (int i = userInput + 1; i < fullNames.Length; i++)
-                {
-                    tempFamily[i - 1] = fullNames[i];
-                    tempPost[i - 1] = positions[i];
-                }
-
-                for (int i = 0; i < tempFamilyDown.Length; i++)
-                {
-                    tempFamilyDown[i] = tempFamily[i];
-                    tempPostDown[i] = tempPost[i];
-                }
-                fullNames = tempFamilyDown;
-                positions = tempPostDown;
+                DecreaseArray(ref fullNames, userInput);
+                DecreaseArray(ref positions, userInput);
                 Console.WriteLine("Досье успешно удалено");
             }
-
             else
             {
                 Console.WriteLine("Досье не найденоно");
             }
             Console.ReadKey();
+        }
+
+        static void DecreaseArray(ref string[] decrease, int inputData)
+        {
+            string[] tempDecrease = new string[decrease.Length - 1];
+
+            for (int i = 0; i < inputData; i++)
+            {
+                tempDecrease[i] = decrease[i];
+            }
+
+            for (int i = inputData + 1; i < decrease.Length; i++)
+            {
+                tempDecrease[i - 1] = decrease[i];
+            }
+            decrease = tempDecrease; ;
         }
 
         static void FindDossier(string[] fullNames, string[] positions)
@@ -141,11 +136,11 @@ namespace Cadrovui_ychet
             Console.WriteLine("   Введите фамилию:  \n");
             string userInput = (Console.ReadLine()).ToUpper();
 
-            foreach (var item in fullNames)
+            foreach (var fullName in fullNames)
             {
-                if (item.Contains(userInput))
+                if (fullName.Contains(userInput))
                 {
-                    int number = Array.IndexOf(fullNames, item);
+                    int number = Array.IndexOf(fullNames, fullName);
                     Console.Clear();
                     Console.WriteLine($" ID - Фамилия -  Имя - Отчество -  Должность ");
                     Console.WriteLine($"Досье найдено: { number } - { fullNames[number] } { positions[number] }");
