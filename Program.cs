8d8a26fd5835234b946342f1edc61fac85490078
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-04-11 02:38:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TryPars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int number;
+            bool isWorking=true;
+
+            while (isWorking)
+            {
+                Console.Clear();
+                Console.WriteLine("\n  Введите число для преобразования:   ");
+                userInput = Console.ReadLine();
+                bool result = int.TryParse(userInput.ToString(), out number);
+                if (result)
+                {
+                    Console.WriteLine($"  Введено число\n   {number} \n  Преобразование удалось");
+                    isWorking = false;  
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("  Вы ввели не число! \n  Повторите");
+                    Console.ReadLine(); 
+                }
+            }
+        }
+    }
+}
