232cb6cd48dd2419c0ab6cb2679f6069420f3684
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-04-13 01:41:27 +0300

    Update Personnel accounting.cs

--- Personnel accounting.cs
+++ Personnel accounting.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Cadrovui_ychet
 {
@@ -6,16 +7,10 @@ namespace Cadrovui_ychet
     {
         static void Main(string[] args)
         {
+            Dictionary<int, string> dossiers = new Dictionary<int, string>();
             string userInput;
-            string family;
-            string name;
-            string post;
-            string[] familyArray = new string[0];
-            string[] nameArray = new string[0];
-            string[] postArray = new string[0];
-            int[] idArray = new int[0];
-            int ID = 0;
             bool isWorking = true;
+            int countDossiers = 0;
 
             while (isWorking)
             {
@@ -23,174 +18,99 @@ namespace Cadrovui_ychet
                 Console.WriteLine("  1 - добавить досье. " +
                     "\n  2 - вывести все досье. " +
                     "\n  3 - удалить досье. " +
-                    "\n  4 - поиск по фамилии. " +
                     "\n  5 - выход");
                 userInput = Console.ReadLine();
 
                 switch (userInput)
                 {
                     case "1":
-                        CreatingDossier(ref familyArray, ref nameArray, ref postArray, ref ID, ref idArray);
+                        AddDossier(ref dossiers, ref countDossiers);
                         break;
+
                     case "2":
-                        ShowAllDossier(familyArray, nameArray, postArray, idArray);
+                        ShowAllDossiers(dossiers);
                         break;
+
                     case "3":
-                        DeleteDossier(ref familyArray, ref nameArray, ref postArray, ref idArray);
+                        dossiers = DeleteDossier(dossiers);
                         break;
+
                     case "4":
-                        FindDossier(familyArray, nameArray, postArray, idArray);
-                        break;
-                    case "5":
                         isWorking = false;
                         break;
+
+                    default:
+                        break;
                 }
+
             }
 
-            static void DeleteDossier(ref string[] familyArray, ref string[] nameArray, ref string[] postArray, ref int[] idArray)
-            {
-                string userInput;
-                bool isFinded = false;
-                Console.Clear();
-                Console.WriteLine("Ведите номер досье: ");
-                userInput = Console.ReadLine();
-
-                for (int id = 0; id < idArray.Length; id++)
-                {
-                    if (Convert.ToInt32(userInput) == id)
-                    {
-                        string[] tempFamily = new string[familyArray.Length];
-                        string[] tempFamilyDown = new string[tempFamily.Length - 1];
-                        string[] tempPost = new string[postArray.Length];
-                        string[] tempPostDown = new string[tempPost.Length - 1];
-                        string[] tempName = new string[nameArray.Length];
-                        string[] tempNameDown = new string[tempName.Length - 1];
-                        int[] tempId = new int[idArray.Length];
-                        int[] tempIdDpwn = new int[idArray.Length - 1];
-
-                        for (int i = 0; i < id; i++)
-                        {
-                            tempFamily[i] = familyArray[i];
-                            tempPost[i] = postArray[i];
-                            tempName[i] = nameArray[i];
-                            tempId[i] = idArray[i];
-                        }
-
-                        for (int i = id + 1; i < familyArray.Length; i++)
-                        {
-                            tempFamily[i - 1] = familyArray[i];
-                            tempPost[i - 1] = postArray[i];
-                            tempName[i - 1] = nameArray[i];
-                            tempId[i - 1] = idArray[i];
-                        }
-
-                        for (int i = 0; i < tempFamilyDown.Length; i++)
-                        {
-                            tempFamilyDown[i] = tempFamily[i];
-                            tempPostDown[i] = tempPost[i];
-                            tempNameDown[i] = tempName[i];
-                            tempIdDpwn[i] = tempId[i];
-                        }
-                        familyArray = tempFamilyDown;
-                        postArray = tempPostDown;
-                        nameArray = tempNameDown;
-                        idArray = tempIdDpwn;
-                        Console.WriteLine("Досье успешно удалено");
-                        isFinded = true;
-                    }
-                }
-
-                if (isFinded == false)
-                {
-                    Console.WriteLine("Досье не найденоно");
-                }
-                Console.ReadKey();
-            }
-            static void FindDossier(string[] familyArray, string[] nameArray, string[] postArray, int[] idArray)
-            {
-
-                    Console.Clear();
-                    Console.WriteLine("   Введите фамилию:  \n");
-                    string family = Console.ReadLine();
-
-                    for (int i = 0; i < familyArray.Length; i++)
-                    {
-                        if (family == familyArray[i])
-                        {
-                            Console.Clear();
-                            Console.WriteLine($" ID - Фамилия -  Имя - Отчество -  Должность ");
-                            Console.WriteLine($"Досье найдено: { idArray[i] } - { familyArray[i] } -  { nameArray[i] } -  { postArray[i] }");
-                        }
-                        else
-                        {
-                            Console.Clear();
-                            Console.WriteLine("  Досье ненайдено!");
-                        }
-                    }
-                    Console.ReadKey();
-                    Console.Clear();
-
-            }
-            static void ShowAllDossier(string[] familyArray, string[] nameArray, string[] postsArray, int[] idArray)
-            {
-                Console.Clear();
-                Console.WriteLine($" ID - Фамилия -  Имя - Отчество -  Должность ");
-                if (familyArray.Length != 0)
-                {
-                    for (int i = 0; i < familyArray.GetLength(0); i++)
-                    {
-
-                        Console.WriteLine($" { idArray[i] } - { familyArray[i] } -  { nameArray[i] } -  { postsArray[i] }");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Досье не найденоно");
-                }
-                Console.ReadKey();
-                Console.Clear();
-            }
-            static void CreatingDossier(ref string[] familyArray, ref string[] nameArray, ref string[] postArray, ref int ID, ref int[] idArray)
-            {
-                string[] tempFamily = new string[familyArray.Length + 1];
-                string[] tempName = new string[nameArray.Length + 1];
-                string[] tempPost = new string[postArray.Length + 1];
-                int[] tempId = new int[idArray.Length + 1];
-                string family;
-                string name;
-                string post;
-                ID++;
-
-                Console.Clear();
-                Console.WriteLine("  Добавление досье \n  Введите фамилию:  \n\n");
-                family = Console.ReadLine();
-                Console.WriteLine("    Введите Имя:  \n\n");
-                name = Console.ReadLine() + " - ";
-                Console.WriteLine("  Введите Отчество:  \n\n");
-                name += Console.ReadLine();
-                Console.WriteLine("  Введите Должность:  \n\n");
-                post = Console.ReadLine();
-
-                for (int i = 0; i < familyArray.GetLength(0); i++)
-                {
-                    tempFamily[i] = familyArray[i];
-                    tempPost[i] = postArray[i];
-                    tempName[i] = nameArray[i];
-                    tempId[i] = idArray[i];
-                }
-                tempFamily[tempFamily.Length - 1] = family;
-                tempName[tempFamily.Length - 1] = name;
-                tempPost[tempPost.Length - 1] = post;
-                tempId[tempId.Length - 1] = ID;
-                familyArray = tempFamily;
-                postArray = tempPost;
-                nameArray = tempName;
-                idArray = tempId;
-                Console.WriteLine("Досье успешно добавлено!");
-                Console.ReadKey();
-            }
         }
+
+        static void AddDossier(ref Dictionary<int, string> tempDossiers, ref int countDossiers)
+        {
+            string namesAndPosition;
+
+            Console.Clear();
+            Console.WriteLine("  Добавление досье \n  Введите фамилию:\n");
+            namesAndPosition = (" - " + Console.ReadLine() + " - ");
+            Console.WriteLine("\n  Введите Имя:\n");
+            namesAndPosition += (Console.ReadLine() + " - ");
+            Console.WriteLine("\n  Введите Отчество:\n");
+            namesAndPosition += (Console.ReadLine() + " - ");
+            Console.WriteLine("\n  Введите Должность:\n");
+            namesAndPosition += Console.ReadLine();
+            tempDossiers.Add(countDossiers, namesAndPosition);
+            countDossiers++;
+            Console.WriteLine("Досье успешно добавлено!");
+            Console.ReadKey();
+        }
+
+        static void ShowAllDossiers(Dictionary<int, string> tempDictionary)
+        {
+            Console.Clear();
+            Console.WriteLine($" ID - Фамилия -  Имя - Отчество -  Должность ");
+
+            foreach (var record in tempDictionary)
+            {
+                Console.WriteLine($"  {record.Key} {record.Value}\n");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static Dictionary<int, string> DeleteDossier(Dictionary<int, string> dossiers)
+        {
+            Dictionary<int, string> tempDossiers = new Dictionary<int, string>();
+
+            Console.Clear();
+            Console.WriteLine("Ведите номер досье: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (dossiers.ContainsKey(userInput) == true)
+            {
+                int count = 0;
+                dossiers.Remove(userInput);
+
+                foreach (var record in dossiers)
+                {
+                    tempDossiers.Add(count, record.Value);
+                    count++;
+                }
+
+                dossiers = tempDossiers;
+                Console.WriteLine("Досье успешно удалено");
+            }
+            else
+            {
+                Console.WriteLine("Досье не найденоно");
+            }
+
+            Console.ReadKey();
+            return dossiers;
+        }
+
     }
+
 }
-
-
