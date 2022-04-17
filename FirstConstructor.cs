91754085797389da9f1e146d0c9351c475af9b49
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-04-17 03:00:34 +0300

    Add files via upload

---
+++ FirstConstructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FirstConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(19, 99, "Андрей");
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        int Age;
+        int Level;
+        string Name; 
+
+
+        public Player(int age,int level,string name)
+        {
+            Age = age;
+            Level = level;
+            Name = name;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Возраст - {Age}, Уровень - {Level}, Имя - {Name}");
+        }
+    }
+        
+}
