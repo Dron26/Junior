1917c422e2dfa154be7d07332f3ea8290c4f57c2
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-04-28 05:23:24 +0300

    Add files via upload

---
+++ Player rendering.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Player_rendering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char playerSymbol = '@';
+
+            Player player = new Player(5,5);
+            Rendering rendering = new Rendering();
+            rendering.Draw(player.PositionX,player.PositionY, playerSymbol);
+        }
+
+    }
+
+    class Player
+    {
+        private int _positionX;
+        private int _positionY;
+        public int PositionX { get { return _positionX; } }
+        public int PositionY { get { return _positionY;} }
+
+        public Player(int positionX,int positionY)
+        {
+            _positionX=positionX;
+            _positionY=positionY;                    
+        }
+
+    }
+
+    class Rendering
+    {
+        public void Draw(int X,int Y, char playerSymbol)
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.WriteLine(playerSymbol);
+        }
+
+    }
+}
