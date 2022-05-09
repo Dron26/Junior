c282636cf5a9c2cb83ef3bc2d8d6976c9bf7b09c
Dron26 <91642819+Dron26@users.noreply.github.com>
2022-05-09 22:40:20 +0300

    Update DeckOfCards.cs

--- DeckOfCards.cs
+++ DeckOfCards.cs
@@ -4,159 +4,176 @@ using System.Collections.Generic;
 namespace deck_of_cards
 {
     internal class Program
-    {
+    {      
         static void Main(string[] args)
         {
             DeckOfCard deckOfCard = new DeckOfCard();
-            Player player = new Player();
+            Player player = new Player(deckOfCard.CountCardInDeck);
             bool isWork = true;
             ConsoleKeyInfo userInput;
+            Program program = new Program();
 
             Console.WriteLine("  Сегодня день фокусов!\n  Достаньте любое количество карт и я угадаю какие вы карты достали!\n  Обещаю не подглядывать!");
             Console.ReadLine();
 
-
             while (isWork)
             {
                 Console.Clear();
-                Console.WriteLine("  Дстать карту -  жмакай Enter\n  Сказать какие ты  взял карты - жмакай Space\n  Выйти - жмакай ESC");
-                userInput = Console.ReadKey(true);
+                Console.WriteLine("  Дстать карту -  жмакай Enter\n  Сказать какие ты  взял карты - жмакай Space\n  " +
+                    $"Выйти - жмакай ESC\n\n" );
+                player.ShowCountCards();
 
+                userInput = Console.ReadKey(true);
+              
                 switch (userInput.Key)
                 {
                     case ConsoleKey.Enter:
-                        isWork = player.TakeCard(deckOfCard.DeckOfCardCount());
+                        program.TryPlayerTakeCard(deckOfCard, player);
                         break;
                     case ConsoleKey.Spacebar:
-                        deckOfCard.ShowGivenCards(player.TakenCards);
+                        player.ShowDeck();
                         break;
                     case ConsoleKey.Escape:
                         isWork = false;
                         break;
                 }
-
             }
-
         }
 
+        private bool TryPlayerTakeCard( DeckOfCard deckOfCard, Player player)
+        {
+            if (deckOfCard.IsCardGiven(out Card card) == true)
+            {
+                player.TakenCard(card);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("ты выгреб всю колоду! Теперь нечего отгадывать))");
+                Console.ReadLine() ;
+                return false;
+            }
+        }
     }
+
+
 
     class DeckOfCard
     {
-        Card card = new Card();
-        private static Dictionary<int, string> Deck = new Dictionary<int, string>();
-        
+        private List<string> Advantage = new List<string> { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
+        private List<string> Suit = new List<string> { "Трефы", "Бубны", "Червы", "Пики", };
+        private static List<Card> Deck = new List<Card>();
+        private Random random = new Random();
+        private int randomNumber;
+        private int countCardTaken = Deck.Count;
+
+        public int CountCardInDeck => Deck.Count;
+
         public DeckOfCard()
         {
-            CreatrDeck();
-            SortDeck();
-        }
-        public int DeckOfCardCount()
-        {
-            int countCardInDeck = Deck.Count;
-            return countCardInDeck;
-        }
-
-        public void CreatrDeck()
-        {
-            int id = 0;
-
-            foreach (string suit in card.Suit)
+            foreach (string suit in Suit)
             {
-                foreach (string advantage in card.Advantage)
+                foreach (string advantage in Advantage)
                 {
-
-                    Deck.Add(id, advantage+ "  "+suit  );
-                    id++;
+                    Deck.Add(new Card(advantage , suit));                    
                 }
             }
+            SortDeck();
         }
 
-        public void SortDeck()
+        public bool IsCardGiven(out Card card)
         {
-            Random random = new Random();
+            randomNumber = random.Next(0, CountCardInDeck);
+            bool isCardGiven = false;
+            card = null;
 
+            if (Deck.Count == 0)
+            {
+                return isCardGiven = false;
+            }
+            else
+            {
+                while (isCardGiven == false)
+                {
+                    if (Deck[randomNumber] != null)
+                    {
+                        card = Deck[randomNumber];
+                        Deck.Remove(card);
+                        isCardGiven = true;
+                    }
+                    else
+                    {
+                        randomNumber = random.Next(0, CountCardInDeck);
+                        isCardGiven = false;
+                    }
+                }
+
+                return isCardGiven;
+            }
+        }
+
+        private void SortDeck()
+        {
             for (int i = 0; i < Deck.Count; i++)
             {
-                int number = random.Next(0, Deck.Count);
-                string valueAdvantage = Deck[i];
-                string valueSuit = Deck[i];
-                Deck[i] = Deck[number];
-                Deck[i] = Deck[number];
-                Deck[number] = valueAdvantage;
-                Deck[number] = valueSuit;
+                randomNumber = random.Next(0, Deck.Count);
+                Card value = Deck[i];
+                Deck[i] = Deck[randomNumber];
+                Deck[randomNumber] = value;
             }
-        }
-
-        public void ShowGivenCards(List<int> TakenCards)
-        {
-            Console.Clear();
-            Console.WriteLine("Ты взял из колоды: ");
-
-            for (int i = 0; i < TakenCards.Count; i++)
-            {
-                Console.WriteLine($"{Deck[TakenCards[i]]} ");
-            }
-
-            Console.ReadLine();
         }
 
     }
 
     class Card
     {
-        public List<string> Advantage = new List<string> { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
-        public List<string> Suit = new List<string> { "Трефы", "Бубны", "Червы", "Пики", };
-
+        public string Advantage { get; }
+        public string Suit { get; }
+        public Card(string advantage, string suit)
+        {
+            Advantage = advantage;
+            Suit = suit;
+        }
     }
 
     class Player
-    {        
-        Random random = new Random();
-        int randomCard;
-        private List<int> _takenCards = new List<int>();
+    {
+        private int _countCardInDeck;
+        private List<Card> _takenCards = new List<Card>();
 
-        public bool TakeCard(int countCardInDeck)
+        public Player(int number)
         {
-            bool isCardTaken = false;
-
-            if (TakenCards.Count == countCardInDeck)
-            {
-                Console.WriteLine("ты выгреб всю колоду! Теперь нечего отгадывать))");
-                return isCardTaken = false;
-            }
-            else
-            {
-                while (isCardTaken == false)
-                {
-                    randomCard = random.Next(0, countCardInDeck);
-
-                    if (TakenCards.Count == 0)
-                    {
-                        TakenCards.Add(randomCard);
-                        isCardTaken = true;
-                    }
-                    else
-                    {
-                        if (!TakenCards.Contains(randomCard))
-                        {
-                            TakenCards.Add(randomCard);
-                            isCardTaken = true;
-                        }
-
-                    }
-
-                }
-
-                return isCardTaken = true;
-            }
-
+            _countCardInDeck= number;
         }
 
-        public List<int> TakenCards
+        public void TakenCard(Card card)
         {
-            get { return _takenCards; }
+            _takenCards.Add(card);
         }
 
+        public void ShowDeck()
+        {
+            Console.Clear();
+
+            foreach (Card card in _takenCards)
+            {
+                Console.WriteLine($"{card.Advantage}-{card.Suit}");
+            }
+
+            Console.ReadLine();
+        }
+
+        public void ShowCountCards()
+        {
+            int count = _takenCards.Count%10;
+            string text = $"У вас в руках {_takenCards.Count}";
+            if (count > 0 && count < 2)
+                text=$"{text} карта";
+            else if (count > 1 && count < 5)
+                text=$"{text} карты";
+            else if (count == 0 || count > 4 && count <= 10)
+                text=$"{text} карт";
+            Console.WriteLine(text);
+        }
     }
 }
